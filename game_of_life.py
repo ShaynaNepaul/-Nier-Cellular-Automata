e3c2f7136136e75c534b19b8ea2eb44fbb99d117
shaynanepaul <shayna.nepaul@gmail.com>
2026-01-25 12:17:06 +0100

    Modfied FOOD with more features and Snake with conditions on arrival cell

--- game_of_life.py
+++ game_of_life.py
@@ -26,41 +26,91 @@ class Map:
                     self.data[i][j] =1 #ça remplace les murs par la valeur 1
                     self.data[i-1][j-1] =1
 
-class Food(Map): 
-
-    def __init__(self):
-        super().__init__()
-        self.stock_food = 10
+class Food:
 
     Cell_status = {
-    "empty": 0,
-    "wall": 1,
-    "food": 3,
-    "trap_1": -1,
-    "queue": -2
-}
+        "empty": 0,
+        "wall": 1,
+        "head" : 2,
+        "food": 3,
+        "food_speed" : 4,
+        "trap_1": -1,
+        "fatal_trap" : -3,
+        "tail": -2
+    }
+
+    def __init__(self, the_map):
+        self.map = the_map          
+        self.stock_food = 10
+
     def add(self, cell_type):
 
-        empty_cell = [
+        """
+        Identifies available empty cells and update the map with the choosen object
+        Returns False if the map is full.
+        
+        :param cell_type: str
+        """
+
+        # get all empty cells from the map
+        # store the coordinate of cells whose value is 0
+        empty_cells = [
             (i, j)
-            for i in range(self.longueur)
-            for j in range(self.longueur)
-            if self.data[i][j] == 0
+            for i in range(self.map.longueur)
+            for j in range(self.map.largeur)
+            if self.map.data[i][j] == self.Cell_status["empty"]
         ]
 
-        i, j = random.choice(empty_cell) #random selection 
-        self.data[i][j] = self.Cell_status[cell_type] # regarding the status that we want of the cell, update diff value
-        if cell_type == "food": 
+        # check whether any cell is available to add something 
+        if not empty_cells:
+            return False
+        
+        # randomly select a pair of coordinates (i, j) from the list of empty cells
+        i, j = random.choice(empty_cells)
+
+        # update the map at the chosen coordinates with the value corresponding to 'cell_type'
+        self.map.data[i][j] = self.Cell_status[cell_type]
+
+    def add_food(self):
+        if self.stock_food > 0 and self.add("food") :
             self.stock_food -= 1
+            return True
+    
+    def add_trap(self, trap_type="trap_1"):
+        return self.add(trap_type)
+    
+    def random_spawn(self):
+
+        # Generate a random float between 0.0 and 1.0
+        param = random.random()
+
+        # If the value is less than 0.4 
+        if param < 0.4 : 
+            self.add_trap()
+        
+        # If the value is less than 0.8 (both this and first if runs)
+        if param < 0.8 : 
+            self.add_trap()
+        
+        # If the value is 0.8 or higher
+        else : 
+            self.add("fatal_trap")
+
+
 
 #snake
+import pygame
+from map import Map
+
 class Snake:
     def __init__(self, the_map):
         self.map = the_map #notre array avec des 0 et des 1
-
         self.head = (self.map.largeur//2, self.map.longueur//2) #démarre au milieu de data
         self.direction = (1,0) #avance vers la droite de base
-
+        self.snake_speed = 15 #snake speed
+        self.alive = True #if the snake is still alive or dead
+        self.life_jauge = 100 #Health as percentage
+        self.score = 0 #player's current score
 
         #changement par un 2 pour la position de base du snake
         self.map.data[self.head[0]][self.head[1]] = 2
@@ -79,12 +129,66 @@ class Snake:
             self.direction = (1,0)
 
     def moove(self): #bouge selon la flèche pressée
+
+        x_before, y_before = self.head #before mouvement
+        new_x, new_y = x_before + self.direction[0], y_before+ self.direction[1] #selon la flèche pressée #arrival cell's coordinate
+
+        arrival_cell =  self.map.data[new_y][new_x] #arrival cell's value
+
+        #Game Over : update self.alive 
+        if arrival_cell in (self.map.Cell_status["wall"], self.map.Cell_status["tail"], self.map.Cell_status["fatal_trap"]): 
+            self.alive = False 
+        
+        #EMPTY CELL : DEFAULT MOUVEMENT
+        if arrival_cell == self.map.Cell_status["empty"]:
+            self.map.data[y_before][x_before] = self.map.Cell_status["empty"] #maintenant c'est un 0 psq on va bouger
+            self.map.data[new_y][new_x] = self.map.Cell_status["head"] #nouvelle position de la tête sur la map
+            self.head = (new_x,new_y) 
+        
+        #FOOD : 
+        if arrival_cell == self.map.Cell_status["food"] : 
+            self.life_jauge += 10
+            self.score += 1
+
+            #MOVE
+            self.map.data[y_before][x_before] = self.map.Cell_status["empty"] #maintenant c'est un 0 psq on va bouger
+            self.map.data[new_y][new_x] = self.map.Cell_status["head"] #nouvelle position de la tête sur la map
+            self.head = (new_x,new_y)
+
+        if arrival_cell == self.map.Cell_status["food_speed"] : 
+            self.life_jauge += 10
+            self.score += 1
+            self.snake_speed += 10
+
+            #MOVE
+            self.map.data[y_before][x_before] = self.map.Cell_status["empty"] 
+            self.map.data[new_y][new_x] = self.map.Cell_status["head"] 
+            self.head = (new_x,new_y)
+
+        #TRAP : lost vitality 
+        if arrival_cell == self.map.Cell_status["trap_1"] : 
+            self.life_jauge -= 10
+            self.score -= 1
+
+            #MOVE
+            self.map.data[y_before][x_before] = self.map.Cell_status["empty"] 
+            self.map.data[new_y][new_x] = self.map.Cell_status["head"] 
+            self.head = (new_x,new_y)
+            #game over if life_jauge drops to 0
+            if self.life_jauge <= 0:
+                self.alive = False
+
+        ##############################
+
+        """
+
         x,y = self.head #position initiale de la tête = 2 sur la map
         self.map.data[y][x] = 0 #maintenant c'est un 0 psq on va bouger
 
         new_x, new_y = x + self.direction[0], y+ self.direction[1] #selon la flèche pressée
 
         self.map.data[new_y][new_x] = 2 #nouvelle position de la tête
+    """
 
 #cells pour test si le snake bouge bien
 
@@ -93,8 +197,6 @@ class Snake:
 get_map = Map()
 
 get_map.walls()
-
-
 
 print(get_map.data)
 
@@ -121,8 +223,6 @@ while not key_pressed:
             key_pressed = True
 
 
-
-
 #%% moove
 
 snake.moove()
@@ -130,41 +230,3 @@ snake.moove()
 #%% nouvelle data
 
 get_map.data
-
-class Game: 
-
-    """
-    Docstring pour Game
-
-    Recuperer la photo 
-    """
-
-    # configuration initiale #
-
-    # position ini snake 
-
-    #
-
-
-
-
-
-#Test 
-
-game_map = Food()
-
-# map avant ajout
-print("=== Avant ajout de la food ===")
-print(game_map.data)
-
-game_map.walls()
-
-# map après murs mais avant food
-print("\n=== Après murs (avant food) ===")
-print(game_map.data)
-
-game_map.add("food")
-
-# map après ajout de la food
-print("\n=== Après ajout de la food ===")
-print(game_map.data)
