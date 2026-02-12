3594b546d13fff3adde2d8c455c8a029c8cbf29a
shaynanepaul <shayna.nepaul@gmail.com>
2026-02-12 22:35:26 +0100

    level update

--- python_project2/levels.py
+++ python_project2/levels.py
@@ -1,8 +1,8 @@
 from food import *
 
-LEVELS = {'1': {'stock_apple': 5, 'traps': [], 'speed' : 4, 'nombre_obstacles' : 0},
-          '2': {'stock_apple' : 6, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 7, 'nombre_obstacles' : 4},
-          '3': {'stock_apple' : 10, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 10, 'nombre_obstacles' : 7},
-          '4': {'stock_apple': 10, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 12, 'nombre_obstacles' : 8},
+LEVELS = {'1': {'stock_apple': 5, 'traps': [], 'speed' : 4, 'nombre_obstacles' : 0, 'frequence' : 10000},
+          '2': {'stock_apple' : 6, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 7, 'nombre_obstacles' : 4, 'frequence' : 10000},
+          '3': {'stock_apple' : 10, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 10, 'nombre_obstacles' : 7, 'frequence' : 8000},
+          '4': {'stock_apple': 10, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 12, 'nombre_obstacles' : 8, 'frequence' : 6000},
           '5': {'stock_apple': 15, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 20},
 }
