c754b60ba58003cc4cbf6c9695f1e92d01ab82a2
shaynanepaul <shayna.nepaul@gmail.com>
2026-02-18 23:23:15 +0100

    last version level

--- python_project2/levels.py
+++ python_project2/levels.py
@@ -1,7 +1,18 @@
 from food import *
 
-LEVELS = {'1': {'stock_apple': 5, 'traps': [], 'speed' : 4, 'nombre_obstacles' : 0, 'frequence' : 10000, "bonus" : "no"},
-          '2': {'stock_apple' : 6, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 6, 'nombre_obstacles' : 4, 'frequence' : 10000, "bonus" : "no"},
-          '3': {'stock_apple' : 15, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 10, 'nombre_obstacles' : 7, 'frequence' : 7000, "bonus" : "yes"},
-          '4': {'stock_apple': 20, 'traps' : [Food.pop_up_bloch_walls], 'speed' : 12, 'nombre_obstacles' : 8, 'frequence' : 6000, "bonus" : "yes"},
+"""
+Configuration dictionary defining the difficulty parameters for each game level.
+
+Each level contains:
+- stock_apple (int): The number of apples required to complete the level.
+- speed (int): The movement of the snake.
+- nombre_obstacles (int): The total count of obstacle blocks to generate with place_obstacles().
+- frequence (int): The time interval (in milliseconds) for spawning new bombs.
+- bonus (str): indicating if 'combo' food is available ("yes"/"no").
+
+"""
+LEVELS = {'1': {'stock_apple': 15, 'speed' : 4, 'nombre_obstacles' : 0, 'frequence_bomb' : 10000, "bonus" : "no", 'portail' : "no"},
+          '2': {'stock_apple' : 30, 'speed' : 6, 'nombre_obstacles' : 4, 'frequence_bomb' : 10000, "bonus" : "no", 'portail' : "no"},
+          '3': {'stock_apple' : 45, 'speed' : 10, 'nombre_obstacles' : 7, 'frequence_bomb' : 7000, "bonus" : "yes", 'portail' : "yes"},
+          
 }
